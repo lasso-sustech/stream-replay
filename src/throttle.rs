b71d9e216a5a277e07245d5f159c34ccc94c9cf5
iamhyc <sudofree@163.com>
2022-10-11 09:58:28 +0800

    use fifo for moving average

--- src/throttle.rs
+++ src/throttle.rs
@@ -1,47 +1,27 @@
-use std::time::{SystemTime};
+use std::time::SystemTime;
+use std::collections::VecDeque;
 
 type TIME = SystemTime;
 type SIZE = usize;
 
-// const SLIDING_WINDOW_SIZE: usize = 200;
-
 struct SlidingWindow<T> {
-    ptr: usize,
-    pub step: usize,
     size: usize,
-    window: Vec<T>
+    pub window: VecDeque<T>
 }
 
 impl<T> SlidingWindow<T>
 where T:Sized + Copy
 {
     pub fn new(size: usize) -> Self {
-        let window = Vec::with_capacity(size);
-        Self{ ptr:0, step:0, size, window }
+        let window = VecDeque::with_capacity(size);
+        Self{ size, window }
     }
 
     pub fn push(&mut self, item: T) {
-        if self.step > 0 {
-            self.ptr = (self.ptr + 1) % self.size;
+        if self.window.len()==self.size {
+            self.window.pop_front();
         }
-        //
-        if self.window.len() < self.size {
-            self.window.push( item );
-        }
-        else {
-            self.window[self.ptr] = item;
-        }
-        //
-        self.step += 1;
-    }
-
-    pub fn get_head(&self) -> T {
-        let index = if self.step > self.size {
-            (self.ptr + 1) % self.size
-        } else {
-            0
-        };
-        self.window[ index ]
+        self.window.push_back(item);
     }
 }
 
@@ -57,7 +37,7 @@ impl RateThrottle {
     }
 
     pub fn exceeds_with(&mut self, size_bytes:usize, interval_ns: u64) -> bool {
-        if self.throttle==0.0 || self.window.step==0 {
+        if self.throttle==0.0 || self.window.window.len()==0 {
             self.window.push(( SystemTime::now(), size_bytes ));
             return false;
         }
@@ -65,7 +45,7 @@ impl RateThrottle {
         let acc_size: usize = self.window.window.iter().map(|&x| x.1).sum();
         let acc_size = acc_size  + size_bytes;
 
-        let acc_time = SystemTime::now().duration_since( self.window.get_head().0 ).unwrap();
+        let acc_time = SystemTime::now().duration_since( self.window.window.get(0).unwrap().0 ).unwrap();
         let acc_time = acc_time.as_nanos() + interval_ns as u128;
 
         let average_rate_mbps = 8.0 * (acc_size as f64/1e6) / (acc_time as f64*1e-9);
