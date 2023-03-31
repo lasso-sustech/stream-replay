68d9d2b17347e13266f5ad6c824bc0ce4a014c98
iamhyc <sudofree@163.com>
2023-03-31 12:39:45 +0800

    still qos failed

--- src/dispatcher.rs
+++ src/dispatcher.rs
@@ -11,7 +11,6 @@ fn create_udp_socket(tos: u8) -> Option<UdpSocket> {
     use std::os::unix::io::AsRawFd;
 
     let sock = UdpSocket::bind("0.0.0.0:0").ok()?;
-    sock.set_nonblocking(true).unwrap();
 
     let res = unsafe{
         let fd = sock.as_raw_fd();
@@ -24,53 +23,70 @@ fn create_udp_socket(tos: u8) -> Option<UdpSocket> {
 }
 
 #[cfg(windows)]
-fn create_udp_socket(_tos: u8) -> Option<UdpSocket> {
-    // use std::os::windows::io::AsRawSocket;
-    // use core::ffi::c_void;
-    // use windows::Win32::Foundation::HANDLE;
-    // use windows::Win32::Networking::WinSock::{WSADATA,SOCKET,WSAStartup};
-    // use windows::Win32::Foundation::GetLastError;
-    // use windows::Win32::NetworkManagement::QoS::{QOS_VERSION, QOSCreateHandle, QOSAddSocketToFlow, QOSSetFlow};
-    // use windows::Win32::NetworkManagement::QoS::{QOS_SET_FLOW, QOSTrafficTypeBestEffort, QOS_NON_ADAPTIVE_FLOW, QOSSetOutgoingDSCPValue};
+fn create_udp_socket(tos: u8) -> Option<UdpSocket> {
+    use std::net::Ipv4Addr;
+    use std::os::windows::prelude::FromRawSocket;
+    use windows::Win32::Foundation::HANDLE;
+    use windows::Win32::Networking::WinSock::{socket,bind,WSADATA,WSAStartup,AF_INET,SOCK_DGRAM,IPPROTO_UDP, SOCKADDR_IN};
+    use windows::Win32::Foundation::GetLastError;
+    use windows::Win32::NetworkManagement::QoS::{QOS_VERSION, QOSCreateHandle, QOSAddSocketToFlow, QOSSetFlow};
+    use windows::Win32::NetworkManagement::QoS::{QOS_SET_FLOW, QOSTrafficTypeBestEffort, QOS_NON_ADAPTIVE_FLOW, QOSSetOutgoingDSCPValue};
 
-    // unsafe {
-    //     let mut wsa = WSADATA::default();
+    unsafe fn print_error_message(prefix:&str) {
+        use windows::core::PWSTR;
+        use windows::Win32::System::Diagnostics::Debug::FormatMessageW;
+        use windows::Win32::System::Diagnostics::Debug::FORMAT_MESSAGE_ALLOCATE_BUFFER;
+        use windows::Win32::System::Diagnostics::Debug::{FORMAT_MESSAGE_FROM_SYSTEM,FORMAT_MESSAGE_IGNORE_INSERTS};
 
-    //     // Initialize WinSock
-    //     if WSAStartup(0x0202, &mut wsa as *mut WSADATA)<0 {
-    //         eprintln!("Initialize WinSock failed ({:?})", GetLastError());
-    //         return None;
-    //     }
+        let dw_flags = FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS;
+        let dw_message_id = GetLastError().0;
+        let mut lp_buffer = PWSTR::null();
 
-    //     // Create socket
-    //     let sock = UdpSocket::bind("0.0.0.0:0").ok()?;
-    //     sock.set_nonblocking(true).unwrap();
-    //     let raw_sock = SOCKET(sock.as_raw_socket() as usize);
+        FormatMessageW(dw_flags, None, dw_message_id, 0, PWSTR(&mut lp_buffer.0 as *mut _ as *mut _), 0, None);
+        assert!( !lp_buffer.is_null() );
+        eprintln!("{}, {}: {}", prefix, dw_message_id, lp_buffer.to_string().unwrap());
+    }
 
-    //     let mut flow_id = 0;
-    //     let mut qos_handle = HANDLE(0);
-    //     let dscp_value = (tos >> 2) as u32; //DSCP value is the high-order 6 bits of the TOS
-    //     let value_size = std::mem::size_of::<u32>();
-    //     let qos_version = QOS_VERSION{ MajorVersion:1, MinorVersion:0 };
+    unsafe {
+        let mut wsa = WSADATA::default();
 
-    //     if !QOSCreateHandle(&qos_version as *const _, &mut qos_handle as *mut HANDLE).as_bool() {
-    //         eprintln!("QOSCreateHandle failed ({:?}).", GetLastError());
-    //         return None;
-    //     }
+        // Initialize WinSock
+        if WSAStartup(0x0202, &mut wsa as *mut WSADATA)<0 {
+            print_error_message("Initialize WinSock Failed");
+            return None;
+        }
 
-    //     if !QOSAddSocketToFlow(qos_handle, raw_sock, None, QOSTrafficTypeBestEffort, QOS_NON_ADAPTIVE_FLOW, &mut flow_id as *mut _ as *mut u32).as_bool() {
-    //         eprintln!("QOSAddSocketToFlow failed ({:?}).",  GetLastError());
-    //         return None;
-    //     }
+        // Create socket
+        let local_addr = "0.0.0.0".parse::<Ipv4Addr>().unwrap().into();
+        let raw_sock = socket(AF_INET.0.into(), SOCK_DGRAM.into(), IPPROTO_UDP.0.into());
+        let local_sockaddr = SOCKADDR_IN{ sin_family:AF_INET, sin_port:0, sin_addr:local_addr, ..Default::default() };
+        bind(raw_sock, &local_sockaddr as *const _ as *const _, std::mem::size_of::<SOCKADDR_IN>() as i32);
 
-    //     if !QOSSetFlow(qos_handle,flow_id,QOSSetOutgoingDSCPValue as QOS_SET_FLOW,value_size as u32,&dscp_value as *const _ as *const c_void,0,None).as_bool() {
-    //         eprintln!("QOSSetFlow failed ({:?}).", GetLastError());
-    //         return None;
-    //     }
-    // }
-    let sock = UdpSocket::bind("0.0.0.0:0").ok()?;
-    sock.set_nonblocking(true).unwrap();
-    Some(sock)
+        // Setup QoS
+        let mut flow_id = 0;
+        let mut qos_handle = HANDLE(0);
+        let dscp_value = (tos >> 2) as u32; //DSCP value is the high-order 6 bits of the TOS
+        let value_size = std::mem::size_of::<u32>();
+        let qos_version = QOS_VERSION{ MajorVersion:1, MinorVersion:0 };
+
+        if !QOSCreateHandle(&qos_version as *const _, &mut qos_handle as *mut HANDLE).as_bool() {
+            print_error_message("QOSCreateHandle Failed");
+            // return None;
+        }
+
+        if !QOSAddSocketToFlow(qos_handle, raw_sock, None, QOSTrafficTypeBestEffort, QOS_NON_ADAPTIVE_FLOW, &mut flow_id as *mut _).as_bool() {
+            print_error_message("QOSAddSocketToFlow Failed");
+            // return None;
+        }
+
+        if !QOSSetFlow(qos_handle,flow_id,QOSSetOutgoingDSCPValue as QOS_SET_FLOW,value_size as u32,&dscp_value as *const _ as *const _,0,None).as_bool() {
+            print_error_message("QOSSetFlow Failed");
+            // return None;
+        }
+
+        let sock = UdpSocket::from_raw_socket( raw_sock.0 as u64 );
+        Some(sock)
+    }
 }
 
 pub struct UdpDispatcher {
@@ -115,6 +131,8 @@ impl UdpDispatcher {
 
 fn dispatcher_thread(rx: PacketReceiver, ipaddr:String, tos:u8, blocked_signal:BlockedSignal) {
     if let Some(sock) = create_udp_socket(tos) {
+        sock.set_nonblocking(true).unwrap();
+
         loop {
             // fetch bulky packets
             let packets:Vec<_> = rx.try_iter().collect();
