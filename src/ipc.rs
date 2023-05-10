6c5182606474ef4c25ccfb6eb9fd2bb28fe057a3
iamhyc <sudofree@163.com>
2023-05-10 18:36:07 +0800

    miscs

--- src/ipc.rs
+++ src/ipc.rs
@@ -30,6 +30,35 @@ impl IPCDaemon {
         Self{ sources, ipc_port }
     }
 
+    fn handle_request(&self, req:Request) -> Response {
+        Response {
+            cmd: req.cmd.clone(),
+            body: match req.cmd.as_str() {
+                "throttle" => {
+                    if let Some(data) = req.body {
+                        let _:Vec<_> = data.iter().map(|(name, value)| {
+                            self.sources[name].throttle(*value);
+                        }).collect();
+                    }
+                    // reset RTT records for all
+                    let _:Vec<_> = self.sources.iter().map(|(_,src)| {
+                        src.reset_rtt_records()
+                    }).collect();
+                    None
+                },
+                "statistics" => {
+                    Some( self.sources.iter().filter_map(|(name,src)| {
+                        match src.statistics() {
+                            Some(stat) => Some(( name.clone(), stat )),
+                            None => None
+                        }
+                    }).collect() )
+                },
+                _ => None
+            }
+        }
+    }
+
     pub fn start_loop(&self, duration:f64) {
         let deadline = SystemTime::now() + Duration::from_secs_f64(duration);
         let addr = format!("0.0.0.0:{}", self.ipc_port);
@@ -41,43 +70,11 @@ impl IPCDaemon {
             if let Ok((len, src_addr)) = sock.recv_from(&mut buf) {
                 let buf_str = std::str::from_utf8(&buf[..len]).unwrap();
                 let req = serde_json::from_str::<Request>(buf_str).unwrap();
-                
-                let res = {
-                    let body = match req.cmd.as_str() {
-                        "throttle" => {
-                            if let Some(data) = req.body {
-                                let _:Vec<_> = data.iter().map(|(name, value)| {
-                                    self.sources[name].throttle(*value);
-                                }).collect();
-                            }
-                            // reset RTT records for all
-                            let _:Vec<_> = self.sources.iter().map(|(_,src)| {
-                                src.reset_rtt_records()
-                            }).collect();
-                            None
-                        },
-                        "statistics" => {
-                            Some( self.sources.iter().filter_map(|(name,src)| {
-                                match src.statistics() {
-                                    Some(stat) => Some(( name.clone(), stat )),
-                                    None => None
-                                }
-                            }).collect() )
-                        },
-                        "stop" => {
-                            break
-                        }
-                        _ => None
-                    };
-                    Response{ cmd:req.cmd.clone(), body }
-                };
-
+                let res = self.handle_request(req);
                 let res = serde_json::to_string(&res).unwrap();
                 sock.send_to(res.as_bytes(), src_addr).unwrap();
             }
-            else {
-                std::thread::sleep( Duration::from_nanos(10_000_000) );
-            }
+            std::thread::sleep( Duration::from_nanos(10_000_000) );
         }
     }
 }
